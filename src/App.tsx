6456a2309b8be1c42f713d55483b36563b161415
Darasimi Oloyede <darasimioloyede@gmail.com>
2023-07-03 12:09:09 +0100

    Finished Homepage

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { NewNote } from "./NewNote";
 import { useLocalStorage } from "./useLocalStorage";
 import { useMemo } from "react";
 import { v4 as uuidV4 } from "uuid";
+import { NoteList } from "./NoteList";
 export type Note = {
 	id: string;
 } & NoteData;
@@ -60,7 +61,12 @@ function App() {
 			<Routes>
 				<Route
 					path="/"
-					element={<h1>Home</h1>}
+					element={
+						<NoteList
+							availableTags={tags}
+							notes={notesWithTags}
+						/>
+					}
 				/>
 				<Route
 					path="/new"
