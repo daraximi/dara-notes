d8cdb82772ae3ab160907e9dc2620a70eb3066af
Darasimi Oloyede <darasimioloyede@gmail.com>
2023-07-03 10:43:15 +0100

    Finished Note form logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,61 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { NewNote } from "./NewNote";
+import { useLocalStorage } from "./useLocalStorage";
+import { useMemo } from "react";
+import { v4 as uuidV4 } from "uuid";
+export type Note = {
+	id: string;
+} & NoteData;
+
+export type RawNote = {
+	id: string;
+} & RawNoteData;
+
+export type RawNoteData = {
+	title: string;
+	markdown: string;
+	tagIds: string[];
+};
+export type NoteData = {
+	title: string;
+	markdown: string;
+	tags: Tag[];
+};
+
+export type Tag = {
+	id: string;
+	label: string;
+};
 
 function App() {
+	const [notes, setNotes] = useLocalStorage<RawNote[]>("notes", []);
+	const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
+	const notesWithTags = useMemo(() => {
+		return notes.map((note) => {
+			return { ...note, tags: tags.filter((tag) => note.tagIds.includes(tag.id)) };
+		});
+	}, [notes, tags]);
+
+	function onCreateNote({ tags, ...data }: NoteData) {
+		setNotes((prevNotes) => {
+			return [
+				...prevNotes,
+				{
+					...data,
+					id: uuidV4(),
+					tagIds: tags.map((tag) => tag.id),
+				},
+			];
+		});
+	}
+	function addTag(tag: Tag) {
+		setTags((prev) => [...prev, tag]);
+	}
+
 	return (
-		<Container>
+		<Container className="my-4">
 			<Routes>
 				<Route
 					path="/"
@@ -13,7 +64,13 @@ function App() {
 				/>
 				<Route
 					path="/new"
-					element={<NewNote/>}
+					element={
+						<NewNote
+							onSubmit={onCreateNote}
+							onAddTag={addTag}
+							availableTags={tags}
+						/>
+					}
 				/>
 				<Route path="/:id">
 					<Route
--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,28 +1,77 @@
+import { FormEvent, useRef, useState } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
+import { NoteData, Tag } from "./App";
+import { v4 as uuidV4 } from "uuid";
 
-export function NoteForm() {
+type NoteFormProps = {
+	onSubmit: (data: NoteData) => void;
+	onAddTag: (tag: Tag) => void;
+	availableTags: Tag[];
+};
+
+export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
+	const titleRef = useRef<HTMLInputElement>(null);
+	const markDownRef = useRef<HTMLTextAreaElement>(null);
+	const [selectedtags, setSelectedTags] = useState<Tag[]>([]);
+	const navigate = useNavigate();
+
+	function handleSubmit(e: FormEvent) {
+		e.preventDefault();
+		onSubmit({
+			title: titleRef.current!.value,
+			markdown: markDownRef.current!.value,
+			tags: selectedtags,
+		});
+
+		navigate("..")
+	}
+
 	return (
-		<Form>
+		<Form onSubmit={handleSubmit}>
 			<Stack gap={4}>
 				<Row>
 					<Col>
 						<Form.Group controlId="title">
 							<Form.Label>Title</Form.Label>
-							<Form.Control required />
+							<Form.Control
+								ref={titleRef}
+								required
+							/>
 						</Form.Group>
 					</Col>
 					<Col>
 						<Form.Group controlId="tags">
 							<Form.Label>Tags</Form.Label>
-							<CreatableReactSelect isMulti />
+							<CreatableReactSelect
+								onCreateOption={(label) => {
+									const newTag = { id: uuidV4(), label };
+									onAddTag(newTag);
+									setSelectedTags((prev) => [...prev, newTag]);
+								}}
+								value={selectedtags.map((tag) => {
+									return { label: tag.label, value: tag.id };
+								})}
+								options={availableTags.map((tag) => {
+									return { label: tag.label, value: tag.id };
+								})}
+								onChange={(tags) => {
+									setSelectedTags(
+										tags.map((tag) => {
+											return { label: tag.label, id: tag.value };
+										})
+									);
+								}}
+								isMulti
+							/>
 						</Form.Group>
 					</Col>
 				</Row>
 				<Form.Group controlId="markdown">
 					<Form.Label>Body</Form.Label>
 					<Form.Control
+						ref={markDownRef}
 						required
 						as="textarea"
 						rows={15}
